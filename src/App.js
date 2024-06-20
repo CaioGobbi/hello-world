5d95b3514852b2d4db0041a66a68a9dd501a5242
caiogobbi <caiogobbi02@gmail.com>
2024-06-20 11:52:17 -0300

    Fundamentos

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World React!</h1>
+      <h2>Hey what's up?</h2>
+      <p>Meu primeiro paragrafo em React!</p>
     </div>
   );
 }
 
 export default App;
+
+
